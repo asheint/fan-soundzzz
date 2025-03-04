ea5384a6ff57718699de387b0b5456bd9ffe105f
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2025-03-04 19:35:43 +0530

    Remove unused audio files and update audio source paths to use uppercase file extensions

--- src/components/AudioController.tsx
+++ src/components/AudioController.tsx
@@ -44,12 +44,12 @@ const AudioController = () => {
     }
     
     // Update audio sources based on fan type
-    startSoundRef.current.src = `./sounds/${fanType}-fan-start.mp3`;
+    startSoundRef.current.src = `./sounds/${fanType}-fan-start.MP3`;
     if (loopSoundRef.current) {
-      loopSoundRef.current.src = `./sounds/${fanType}-fan-loop.mp3`;
+      loopSoundRef.current.src = `./sounds/${fanType}-fan-loop.MP3`;
     }
     if (stopSoundRef.current) {
-      stopSoundRef.current.src = `./sounds/${fanType}-fan-stop.mp3`;
+      stopSoundRef.current.src = `./sounds/${fanType}-fan-stop.MP3`;
     }
     
     // Set loop property
